93ed2ccc8358d1047d479a283ae1cf5401b61bfe
Dawid Żurawski <d.zurawski@merixstudio.com>
2019-11-28 22:26:20 +0100

    Registered models to admin.

--- cosmetics/admin.py
+++ cosmetics/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 
 from .models import (
     BodyCosmetic,
+    CosmeticProperties,
     FaceCosmetic,
     FeetCosmetic,
     HairCosmetic,
     HandsCosmetic,
+    Ingredient,
+    IngredientNames,
 )
 
 admin.site.register(BodyCosmetic)
+admin.site.register(CosmeticProperties)
 admin.site.register(FaceCosmetic)
 admin.site.register(FeetCosmetic)
 admin.site.register(HairCosmetic)
 admin.site.register(HandsCosmetic)
+admin.site.register(Ingredient)
+admin.site.register(IngredientNames)
