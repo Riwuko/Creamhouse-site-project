161ba48bb8a27dfca16e87b9dbc623571129b3be
Dawid Żurawski <d.zurawski@merixstudio.com>
2019-12-03 19:57:33 +0100

    some changes

--- cosmetics/static/cosmetics/scripts/addIngredients.js
+++ cosmetics/static/cosmetics/scripts/addIngredients.js
@@ -1,11 +1,48 @@
-const addRelatedName = (ingredientElement) => {
-  console.log('asdasd')
+const addRelatedName = ingredientElement => {
   const newRelatedName = `
     <div class="single-ingredient-name">
         <label for="name">Related Name:</label>
         <input type="text" name="ingredient-name">
     </div>`;
-  ingredientElement.innerHTML += newRelatedName;
+  const div = document.createElement('div');
+  div.innerHTML = newRelatedName;
+  ingredientElement.parentElement.appendChild(div);
 };
 
-document.querySelector('input.add-related-name').addEventListener("click", () => {addRelatedName(document.querySelector('div.single-ingredient'))});
+document
+  .querySelector('input.add-related-name')
+  .addEventListener('click', () => {
+    addRelatedName(document.querySelector('div.single-ingredient'));
+  });
+
+const requestPost = async (url, jsonData) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': Cookies.get('csrftoken'),
+    },
+    body: JSON.stringify(jsonData),
+    credentials: 'include',
+  });
+  return response.json();
+};
+
+const getIngredientNames = () => {
+  const ingredientNames = [];
+  for (input of document.querySelectorAll('div.single-ingredient-name input')) {
+    ingredientNames.push(input.value);
+  }
+  return ingredientNames;
+};
+
+const addIngredient = async () => {
+  const jsonData = getIngredientNames();
+  const resp = await requestPost('/ingredient/add', jsonData);
+  console.log(resp);
+  window.location.replace('/')
+};
+
+document
+  .querySelector('form button')
+  .addEventListener('click', addIngredient);
--- cosmetics/views.py
+++ cosmetics/views.py
@@ -1,38 +1,36 @@
+import json
 from functools import wraps
 
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 import jsonschema
 from django.utils.decorators import method_decorator
 
-ingredient_schema = {
-	"type":"array",
-    "names": {
-        "type":"string"
-    }
-}
+ingredient_schema = {"type": "array", "names": {"type": "string"}}
+
 
 def validate_ingredients_json(func):
-	    @wraps(func)
-	    def wrapper(request, *args, **kwargs):
-	        try:
-	            requested_ingredients = json.loads(request.body)
-	            jsonschema.validate(requested_ingredients, ingredient_schema)
-	            return func(
-	                *args,
-	                request=request,
-	                requested_ingredients=requested_ingredients,
-	                **kwargs
-	            )
-	        except jsonschema.exceptions.ValidationError as exc:
-	            error_message = exc.message
-	        except json.decoder.JSONDecodeError:
-	            error_message = 'Missing JSON in request.'
-	        return JsonResponse(
-	            {'status': 400, 'error_message': error_message}, status=400
-	        )
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        try:
+            requested_ingredients = json.loads(request.body)
+            jsonschema.validate(requested_ingredients, ingredient_schema)
+            return func(
+                *args,
+                request=request,
+                requested_ingredients=requested_ingredients,
+                **kwargs
+            )
+        except jsonschema.exceptions.ValidationError as exc:
+            error_message = exc.message
+        except json.decoder.JSONDecodeError:
+            error_message = 'Missing JSON in request.'
+        return JsonResponse(
+            {'status': 400, 'error_message': error_message}, status=400
+        )
 
-	    return wrapper
+    return wrapper
 
 
 class AddNewIngredient(View):
@@ -40,13 +38,16 @@ class AddNewIngredient(View):
         context = {}
         return render(request, 'cosmetic/ingredient_create.html', context)
 
-    
-
-
     @method_decorator(validate_ingredients_json)
-    def post(self,request, requested_ingredients):
-    	for i in range (len(requested_ingredients)):
-    		if (i==0):
-    			main_ingredient = Ingredients.objects.create(main_name=requested_ingredients[i]["name"])
-    		else:
-    			IngredientNames.objects.create(main_ingredient, name=requested_ingredients[i]["name"])
+    def post(self, request, requested_ingredients):
+        print(requested_ingredients)
+        return JsonResponse({'message': ':)'})
+        # for i in range(len(requested_ingredients)):
+        #     if i == 0:
+        #         main_ingredient = Ingredients.objects.create(
+        #             main_name=requested_ingredients[i]["name"]
+        #         )
+        #     else:
+        #         IngredientNames.objects.create(
+        #             main_ingredient, name=requested_ingredients[i]["name"]
+        #         )
