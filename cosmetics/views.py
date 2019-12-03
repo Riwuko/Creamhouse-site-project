5e92402c75e2e73fb0bdce69db0a48113165e4f1
riwuko x <riwuko@gmail.com>
2019-12-03 18:59:57 +0100

    json validation-stuff

--- cosmetics/views.py
+++ cosmetics/views.py
@@ -1,5 +1,38 @@
+from functools import wraps
+
 from django.shortcuts import render
 from django.views import View
+import jsonschema
+from django.utils.decorators import method_decorator
+
+ingredient_schema = {
+	"type":"array",
+    "names": {
+        "type":"string"
+    }
+}
+
+def validate_ingredients_json(func):
+	    @wraps(func)
+	    def wrapper(request, *args, **kwargs):
+	        try:
+	            requested_ingredients = json.loads(request.body)
+	            jsonschema.validate(requested_ingredients, ingredient_schema)
+	            return func(
+	                *args,
+	                request=request,
+	                requested_ingredients=requested_ingredients,
+	                **kwargs
+	            )
+	        except jsonschema.exceptions.ValidationError as exc:
+	            error_message = exc.message
+	        except json.decoder.JSONDecodeError:
+	            error_message = 'Missing JSON in request.'
+	        return JsonResponse(
+	            {'status': 400, 'error_message': error_message}, status=400
+	        )
+
+	    return wrapper
 
 
 class AddNewIngredient(View):
@@ -7,3 +40,13 @@ class AddNewIngredient(View):
         context = {}
         return render(request, 'cosmetic/ingredient_create.html', context)
 
+    
+
+
+    @method_decorator(validate_ingredients_json)
+    def post(self,request, requested_ingredients):
+    	for i in range (len(requested_ingredients)):
+    		if (i==0):
+    			main_ingredient = Ingredients.objects.create(main_name=requested_ingredients[i]["name"])
+    		else:
+    			IngredientNames.objects.create(main_ingredient, name=requested_ingredients[i]["name"])
