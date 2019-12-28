24234d342a38c58d6602ee7582dde1349f8a9a7d
Dawid Żurawski <d.zurawski@merixstudio.com>
2019-12-28 15:30:24 +0100

    Changed signup view to class based.

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from accounts.views import signup
+from accounts.views import SignUp
 
 app_name = 'accounts'
 
@@ -26,7 +26,7 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name='accounts/login.html'),
         name='login',
     ),
-    path('signup/', signup, name='signup'),
+    path('signup/', SignUp.as_view(), name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path(
         'password-reset',
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render, redirect
-
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('cosmetics:home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+class SignUp(FormView):
+    template_name = 'accounts/signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('cosmetics:home')
+
+    def form_valid(self, form):
+        form.save()
+        self.login_user(form)
+        return super().form_valid(form)
+
+    def login_user(self, form):
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
